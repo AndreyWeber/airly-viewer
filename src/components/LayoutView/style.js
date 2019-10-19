d12ddecbaaa2a32429460073ac84cc4db8c10b9b
Andrey Weber <andrey.weber@gmail.com>
2019-10-19 13:38:24 +0200

    #23: Changed style of component to correctly show body and bottom sections

--- src/components/LayoutView/style.js
+++ src/components/LayoutView/style.js
@@ -3,18 +3,20 @@ import createStyles, { dimensions } from '../../styles/base';
 const layoutViewStyles = createStyles({
     root: {
         display: 'flex',
-        width: dimensions.maxWidth,
-        height: dimensions.maxHeight
+        flexDirection: 'column',
+        width: dimensions.relMaxWidth,
+        height: dimensions.relMaxHeight
     },
     body: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexGrow: 1 // 'body' area must be bigger than 'bottom' area
+        // borderWidth: 2,
+        // borderColor: 'red',
+        height: '80%',
+        flexGrow: 1 // 'body' area must be higher than 'bottom' area
     },
     bottom: {
-        flexGrow: 0 // 'bottom' area must be lesser than 'body arae'
+        // borderWidth: 2,
+        // borderColor: 'yellow',
+        flexGrow: 0 // 'bottom' area must be less high than 'body arae'
     }
 });
 
